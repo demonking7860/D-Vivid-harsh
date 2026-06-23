50c2da68dbdb424376c1eb3fc03049bd898a8e2b
Shashwat Shahi <shashwat.2226cseai11@kiet.edu>
2026-06-23 18:12:20 +0530

    updating university logo

--- src/components/marketing/companies.tsx
+++ src/components/marketing/companies.tsx
@@ -1,5 +1,13 @@
-import Images from "../global/images";
 import Marquee from "../ui/marquee";
+
+const universities = [
+    { name: "University of Nebraska", logo: "/universities/nebraska.png" },
+    { name: "Concordia University", logo: "/universities/concordia.png" },
+    { name: "Pace University", logo: "/universities/pace.png" },
+    { name: "University of Illinois Chicago", logo: "/universities/uic.png" },
+    { name: "Arizona State University", logo: "/universities/asu.png" },
+    { name: "UMass Boston", logo: "/universities/umass-boston.png" },
+];
 
 const Companies = () => {
     return (
@@ -10,14 +18,20 @@ const Companies = () => {
                 </h2>
                 <div className="mt-16 w-full relative overflow-hidden">
                     <Marquee pauseOnHover className="[--duration:30s]">
-                        <div className="flex gap-8 md:gap-12">
-                            <Images.university1 className="w-32 h-8" />
-                            <Images.university2 className="w-32 h-8" />
-                            <Images.university3 className="w-32 h-8" />
-                            <Images.university4 className="w-32 h-8" />
-                            <Images.university5 className="w-32 h-8" />
-                            <Images.university6 className="w-32 h-8" />
-                            <Images.university7 className="w-32 h-8" />
+                        <div className="flex gap-6 md:gap-8">
+                            {universities.map((university) => (
+                                <div
+                                    key={university.name}
+                                    className="flex items-center justify-center h-20 w-44 shrink-0 rounded-xl bg-white px-6 py-4 shadow-sm"
+                                >
+                                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                                    <img
+                                        src={university.logo}
+                                        alt={`${university.name} logo`}
+                                        className="max-h-full max-w-full object-contain"
+                                    />
+                                </div>
+                            ))}
                         </div>
                     </Marquee>
                     <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background"></div>
